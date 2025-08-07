3c71b825ddba0b99a6205b76d85fa3556b420fa0
Awez  Sheikh <91287127+Awezsk@users.noreply.github.com>
2025-08-08 03:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+// Highlight the link briefly when clicked
+document.getElementById("song-link").addEventListener("click", function () {
+  this.style.backgroundColor = "#ffd633";
+  this.style.color = "#222";
+
+  setTimeout(() => {
+    this.style.backgroundColor = "";
+    this.style.color = "";
+  }, 500);
+});
